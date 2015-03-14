a9bcf61b7d52abf77f12d7436a96469bfc8c72a9
Mohamad El-Husseini <husseini.mel@gmail.com>
2015-03-13 22:41:52 -0300

    Create dummy_users.rb

---
+++ dummy_users.rb
@@ -0,0 +1,17 @@
+users = [
+  { name: "Marg Simpson", email: "marg@simpson.com", password: "12121212", admin: true },
+  { name: "Lisa Simpson", email: "lisa@simpson.com", password: "12121212" },
+  { name: "Bart Simpson", email: "bart@simpson.com", password: "12121212" },
+  { name: "Maggie Simpson", email: "maggie@simpson.com", password: "12121212" },
+  { name: "Montgomery Burns", email: "monty@burns.com", password: "12121212" },
+  { name: "Nelson Muntz", email: "nelson@muntz.com", password: "12121212" },
+  { name: "Ned Flanders", email: "ned@flanders.com", password: "12121212" },
+  { name: "Moe Szyslak", email: "moe@szyslak.com", password: "12121212" },
+  { name: "Milhouse Van Houten", email: "milhouse@houten.com", password: "12121212" },
+  { name: "Krusty the Clown", email: "crusty@clown.com", password: "12121212" },
+  { name: "Barney Gumble", email: "barney@gumble.com", password: "12121212" },
+  { name: "Waylon Smithers", email: "waylon@smithers.com", password: "12121212" },
+  { name: "Clarence Wiggum", email: "clarence@wiggum.com", password: "12121212" },
+]
+
+User.create(users)
